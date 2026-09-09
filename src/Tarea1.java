b2c8ee90f44465bf39826a525c512d941763612a
andresccastelblancod-oss <andresc.castelblancod@uqvirtual.edu.co>
2026-09-09 18:20:01 -0500

    cambio 1

--- src/Tarea1.java
+++ src/Tarea1.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class Tarea1 {
+    int numero1= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 1"));
+    int numero2= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 1"));
+
 }
