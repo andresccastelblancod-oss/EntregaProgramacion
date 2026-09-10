46549762f34ff3ddcc4c0725d440a73fb62c5f45
ANDRES CAMILO CASTELBLANCO DUQUE <andresc.castelblancod@uqvirtual.edu.co>
2026-09-09 22:05:33 -0500

    Update Tarea1.java

--- src/Tarea1.java
+++ src/Tarea1.java
@@ -7,6 +7,7 @@ public class Tarea1 {
         int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 1"));
         int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 2"));
         int suma=numero1+numero2;
+        int resta=numero1-numero2;
 
         checkout status, add, commit, push, pull, branch.
 
