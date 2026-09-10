251dc4edbb60d0ec8ebb214424c05c926989f0ee
andresccastelblancod-oss <andresc.castelblancod@uqvirtual.edu.co>
2026-09-09 21:41:54 -0500

    Quitar

--- src/Tarea1.java
+++ src/Tarea1.java
@@ -6,9 +6,7 @@ public class Tarea1 {
     public static void main(String[] args) {
         int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 1"));
         int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 2"));
-        int suma=numero1+numero2;
 
-        checkout status, add, commit, push, pull, branch.
 
     }
 }
