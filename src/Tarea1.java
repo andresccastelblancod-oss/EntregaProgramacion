f629bd1eb15747da562860f8f332f02d14065fd2
ANDRES CAMILO CASTELBLANCO DUQUE <andresc.castelblancod@uqvirtual.edu.co>
2026-09-09 20:18:28 -0500

    Fix variable initialization in Tarea1.java

--- src/Tarea1.java
+++ src/Tarea1.java
@@ -3,5 +3,5 @@ import javax.swing.*;
 public class Tarea1 {
     int numero1= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 1"));
     int numero2= Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 1"));
-    int suma=
+    int resta=
 }
