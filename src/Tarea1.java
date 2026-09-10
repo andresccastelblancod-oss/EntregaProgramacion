0933bad6d8dc6d1cde188d6c08bace0080f710ec
andresccastelblancod-oss <andresc.castelblancod@uqvirtual.edu.co>
2026-09-09 20:32:10 -0500

    Inicializacion de prueba

--- src/Tarea1.java
+++ src/Tarea1.java
@@ -5,8 +5,6 @@ public class Tarea1 {
         int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 1"));
         int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 2"));
 
-        int suma = numero1 + numero2;
 
-        JOptionPane.showMessageDialog(null, "La suma es: " + suma);
     }
 }
