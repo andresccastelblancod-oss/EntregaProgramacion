452a7bf816a4516c17197384ea6524ac58dd0eb5
andresccastelblancod-oss <andresc.castelblancod@uqvirtual.edu.co>
2026-09-09 21:16:13 -0500

    Añadir variable de resta

--- src/Tarea1.java
+++ src/Tarea1.java
@@ -1,10 +1,13 @@
 import javax.swing.JOptionPane;
 
+import static javax.swing.JOptionPane.showMessageDialog;
+
 public class Tarea1 {
     public static void main(String[] args) {
         int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 1"));
         int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero 2"));
         int suma= numero1+numero2;
+        int resta=numero1-numero2;;
 
     }
 }
